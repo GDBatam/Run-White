23a3a253cf9c3efa059676a018048591a3d7bbdc
Johan Fayt <fayt12@yahoo.com>
2022-08-13 15:35:16 +0700

    snake controller mechanic

--- Assets/Scripts/Runtime/Gameplay/GameManager.cs
+++ Assets/Scripts/Runtime/Gameplay/GameManager.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> snakePart;
+    public Transform startingPosition;
+    public SnakeController controller;
     // Start is called before the first frame update
     void Start()
     {
-        
+        controller.snake.Add(Instantiate(snakePart[0], startingPosition));
     }
 
     // Update is called once per frame
