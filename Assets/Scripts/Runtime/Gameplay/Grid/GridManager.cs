7771eb0166abde4e727ffe17ceca91e589cde916
Eusford08 <Jacksonlee850@gmail.com>
2022-08-13 16:19:56 +0700

    Snake-Controller-Mechanic
    #1

--- Assets/Scripts/Runtime/Gameplay/Grid/GridManager.cs
+++ Assets/Scripts/Runtime/Gameplay/Grid/GridManager.cs
@@ -2,8 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Tile
+{
+    public int x;
+
+    public int y;
+
+    public GameObject prefab;
+}
+
 public class GridManager : MonoBehaviour
 {
+    #region Fields
 
     [SerializeField]
     private GridSetting setting;
@@ -11,28 +21,61 @@ public class GridManager : MonoBehaviour
     [SerializeField] 
     private GameObject prefabTile;
 
+    public List<GameObject> grids = new List<GameObject>();
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        GenerateGrid();
-    }
+    private Tile[,] tiles;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    #endregion
+
+    #region Properties
+
+    public static GridManager Singleton { get; private set; }
+
+    #endregion
+
+    #region Methods
 
     private void GenerateGrid()
     {
+        tiles = new Tile[setting.width, setting.height];
+
         for(int i=0;i < setting.width; i++)
         {
             for (int j = 0; j < setting.height; j++)
             {
                 GameObject newTile = Instantiate<GameObject>(prefabTile);
                 newTile.transform.position = new Vector3(i, j, 0);
+                grids.Add(newTile);
+                tiles[i,j] = new Tile; 
             }
         }
     }
+
+    #endregion
+    
+    #region Build-in Methods
+    
+    private void Awake()
+    {
+        if (Singleton != this && Singleton != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Singleton = this;
+    }
+
+    //// Start is called before the first frame update
+    private void Start()
+    {
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+
+    #endregion
 }
--- Assets/Scripts/Runtime/Gameplay/Snake/SnakeController.cs
+++ Assets/Scripts/Runtime/Gameplay/Snake/SnakeController.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class Entity
+{
+    public Transform position;
+
+    public GameObject skin;
+}
+
+
 public class SnakeController : MonoBehaviour
 {
     List<GameObject> targetGrid;
-    List<GameObject> snake;
+    public List<GameObject> snake;
+
+    public List<Entity> snakes = new List<Entity>();
+
+    public void SetVector2(Vector2 targetPosition)
+    {
+        
+    }
+
+
     // Start is called before the first frame update
     void Start()
     {
